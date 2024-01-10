c6bf734c644917c0af0b849c1b143b3c3c30c4ff
Garbi93 <iseung2093@gmail.com>
2024-01-10 11:55:55 +0900

    테스트 코드 연습중

--- src/main/java/com/example/lollandback/member/controller/MemberController.java
+++ src/main/java/com/example/lollandback/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.example.lollandback.member.controller;
 
 import com.example.lollandback.member.domain.Member;
 import com.example.lollandback.member.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +17,7 @@ public class MemberController {
     private final MemberService service;
 
     @PostMapping("signUp")
-    public void addUser(@RequestBody Member member) {
+    public void addUser(@RequestBody @Valid Member member) {
         service.addUser(member);
     }
 }
--- src/main/java/com/example/lollandback/member/domain/Member.java
+++ src/main/java/com/example/lollandback/member/domain/Member.java
@@ -1,12 +1,15 @@
 package com.example.lollandback.member.domain;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
 public class Member {
+    @NotBlank(message = "회원 아이디가 빈 값입니다.")
     private String member_login_id;
+    @NotBlank(message = "비밀번호가 빈 값입니다.")
     private String member_password;
     private String member_name;
     private String member_phone_number;
