1eba23b1b49d66dbb1becb000ea0919b2c851c6d
smil98 <seungmilee98@gmail.com>
2024-01-10 19:04:20 +0900

    Cart : 백엔드 작업 중

--- src/main/java/com/example/lollandback/board/cart/controller/CartController.java
+++ src/main/java/com/example/lollandback/board/cart/controller/CartController.java
@@ -1,4 +1,22 @@
 package com.example.lollandback.board.cart.controller;
 
+import com.example.lollandback.board.cart.dto.CartDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/cart")
 public class CartController {
+
+    @GetMapping("/fetch")
+    List<CartDto> fetchCart(@RequestParam Long member_id) {
+
+        return null;
+    }
 }
--- src/main/java/com/example/lollandback/board/cart/dto/CartDto.java
+++ src/main/java/com/example/lollandback/board/cart/dto/CartDto.java
@@ -1,4 +1,18 @@
 package com.example.lollandback.board.cart.dto;
 
+import lombok.Data;
+
+@Data
 public class CartDto {
+    private Long cart_id;
+    private Long member_id;
+    private Long product_id;
+    private String member_login_id;
+    private String category_name;
+    private String subcategory_name;
+    private String product_name;
+    private String company_name;
+    private String main_img_url;
+    private Double product_price;
+    private Integer count;
 }
