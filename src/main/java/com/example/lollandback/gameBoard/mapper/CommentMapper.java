4ca54fa14d517fd83cd68dad2e2ac6c90f7074e0
CHOHUNE <yuureru@gmail.com>
2024-01-12 11:20:37 +0900

    1/11 대댓글 ..완성...

--- src/main/java/com/example/lollandback/gameBoard/mapper/CommentMapper.java
+++ src/main/java/com/example/lollandback/gameBoard/mapper/CommentMapper.java
@@ -21,50 +21,56 @@ VALUES (#{comment_content},#{game_board_id},#{parent_id})
 //""")
 //    List<Comment> selectByBoardId(Integer boardId);
 
-@Select("""
-        WITH RECURSIVE CommentHierarchy AS (
-            SELECT
-              id,
-              comment_content,
-              reg_time,
-              parent_id,
-              game_board_id,
-              member_id,
-              1 AS depth
-            FROM
-              gameboardcomment
-            WHERE
-              parent_id IS NULL
-            UNION ALL
-            SELECT
-              c.id,
-              c.comment_content,
-              c.reg_time,
-              c.parent_id,
-              c.game_board_id,
-              c.member_id,
-              ch.depth + 1 AS depth
-            FROM
-              gameboardcomment c
-            JOIN
-              CommentHierarchy ch ON c.parent_id = ch.id
-        )
-        SELECT
-          id,
-          comment_content,
-          reg_time,
-          parent_id,
-          game_board_id,
-          member_id,
-          depth
-        FROM
-          CommentHierarchy
-        WHERE
-          game_board_id = #{game_board_id}
-        ORDER BY
-          parent_id,reg_time
-        """)
-        List<Comment> selectByBoardId(Integer game_board_id);
+    @Select("""
+
+            WITH RECURSIVE CommentHierarchy AS (
+                   SELECT
+                       id,
+                       comment_content,
+                       reg_time,
+                       parent_id,
+                       game_board_id,
+                       member_id,
+                       CAST(id AS CHAR) AS path,
+                       1 AS depth
+                   FROM
+                       gameboardcomment
+                   WHERE
+                       parent_id IS NULL -- 최상위 댓글은 parent_id가 NULL인 것으로 시작
+               
+                   UNION ALL
+               
+                   SELECT
+                       c.id,
+                       c.comment_content,
+                       c.reg_time,
+                       c.parent_id,
+                       c.game_board_id,
+                       c.member_id,
+                       CONCAT(ch.path, ',', c.id),
+                       ch.depth + 1 AS depth
+                   FROM
+                       gameboardcomment c
+                   JOIN
+                       CommentHierarchy ch ON c.parent_id = ch.id
+               )
+               
+               SELECT
+                   id,
+                   comment_content,
+                   reg_time,
+                   parent_id,
+                   game_board_id,
+                   member_id,
+                   depth
+               FROM
+                   CommentHierarchy
+               ORDER BY
+                   path;
+""")
+    List<Comment> selectByBoardId(Integer game_board_id);
+
+
 
 
 
