1240e0bb1f5335718247b508d1684f299d2c4077
smil98 <seungmilee98@gmail.com>
2024-01-13 17:06:04 +0900

    Cart: deleteSelected IN 인식 오류라 주석처리 CartMapper: 주석처리

--- src/main/java/com/example/lollandback/board/cart/mapper/CartMapper.java
+++ src/main/java/com/example/lollandback/board/cart/mapper/CartMapper.java
@@ -37,14 +37,14 @@ public interface CartMapper {
     """)
     void deleteByCartId(Long cartId);
 
-    @Delete("""
-        DELETE FROM cart
-        WHERE cart_id IN
-        <foreach collection="cartIds" open="(" seperator="," close=")">
-            #{cart_id}
-        </foreach>
-    """)
-    void deleteSelected(List<Long> cartIds);
+//    @Delete("""
+//        DELETE FROM cart
+//        WHERE cart_id IN
+//        <foreach collection="cartIds" open="(" separator="," close=")">
+//            #{cartId}
+//        </foreach>
+//    """)
+//    void deleteSelected(List<Long> cartIds); //TODO: IN이 인식이 안된다
 
     @Delete("""
         DELETE FROM cart
