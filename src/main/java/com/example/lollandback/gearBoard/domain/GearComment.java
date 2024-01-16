eb5274059136aed0bc24bd98579500784c6c8712
jaehee <cjhee0601@naver.com>
2024-01-16 18:02:14 +0900

    1/16일

--- src/main/java/com/example/lollandback/gearBoard/domain/GearComment.java
+++ src/main/java/com/example/lollandback/gearBoard/domain/GearComment.java
@@ -1,18 +1,18 @@
-package com.example.lollandback.gearBoard.domain;
-
-import lombok.Data;
-
-import java.time.LocalDateTime;
-
-@Data
-public class GearComment {
-
-    private  Integer id;
-    private  Integer gear_id;
-    private Integer memberId;
-    private  String  comment;
-    private LocalDateTime inserted;
-
-
-
-}
+//package com.example.lollandback.gearBoard.domain;
+//
+//import lombok.Data;
+//
+//import java.time.LocalDateTime;
+//
+//@Data
+//public class GearComment {
+//
+//    private  Integer id;
+//    private  Integer gear_id;
+//    private Integer memberId;
+//    private  String  comment;
+//    private LocalDateTime inserted;
+//
+//
+//
+//}
--- src/main/java/com/example/lollandback/gearBoard/mapper/GearCommentMapper.java
+++ src/main/java/com/example/lollandback/gearBoard/mapper/GearCommentMapper.java
@@ -1,14 +1,14 @@
-package com.example.lollandback.gearBoard.mapper;
-
-import com.example.lollandback.gearBoard.domain.GearComment;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface GearCommentMapper {
-
-    @Insert("""
-            insert into comment (gear_id, comment, memberId) values (#{gear_id},#{comment},#{memberId});
-        """)
-     int add(GearComment gearComment);
-}
+//package com.example.lollandback.gearBoard.mapper;
+//
+//import com.example.lollandback.gearBoard.domain.GearComment;
+//import org.apache.ibatis.annotations.Insert;
+//import org.apache.ibatis.annotations.Mapper;
+//
+//@Mapper
+//public interface GearCommentMapper {
+//
+//    @Insert("""
+//            insert into comment (gear_id, comment, memberId) values (#{gear_id},#{comment},#{memberId});
+//        """)
+//     int add(GearComment gearComment);
+//}
